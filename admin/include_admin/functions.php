52f817479b853b628f6427c30d3b861455754a37
Set Tun Shein <setwebdev@gmail.com>
2022-10-23 23:57:22 +0630

    PHP BLOG

--- admin/include_admin/functions.php
+++ admin/include_admin/functions.php
@@ -302,6 +302,7 @@ function insert_post()
         $file_name   = $_FILES['image']['name'];
         $file_temp   = $_FILES['image']['tmp_name'];
         if( isset($file_name) ) {
+            $file_name = uniqid(time()) . $file_name;
             move_uploaded_file($file_temp, "uploads/post/$file_name");
         }
     
